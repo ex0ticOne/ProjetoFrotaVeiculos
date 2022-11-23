ee15d50306e33a2c8b21c98ee4a6feda9a60aa70
ex0ticOne <rricardorb@gmail.com>
2022-11-23 10:42:16 -0300

    LEIAME e atualizacao script

--- script.R
+++ script.R
@@ -5,27 +5,38 @@ library(dygraphs)
 library(dplyr)
 library(htmltools)
 library(readr)
-library(tidyr)
 
+argumentos <- commandArgs(trailingOnly = TRUE)
+
+n_periodos <- as.numeric(argumentos[1])
+
+#Prophet com algumas traduções para português brasileiro
 source('ProphetPersonalizado.R')
 
+#Carrega o dataset
 dataset_frota <- read_csv('dataset_frota.csv')
 
+#Cria as relações de UFs e Categorias de veículos para o loop
 lista_UF <- data.frame(table(dataset_frota$UF))
 lista_UF <- as.factor(lista_UF$Var1)
 
 lista_tipo <- data.frame(table(dataset_frota$TIPO))
 lista_tipo <- as.factor(lista_tipo$Var1)
 
+#Cria o dataset agrupado por UF
 dataset_frota_UF <- dataset_frota %>%
   group_by(UF, TIPO, ds) %>%
   summarise(y = sum(y)) %>%
   mutate(diferenca = y - lag(y))
 
+#Cria um dataset de média de aumento de veículos por categoria por mês
 media_aumento_frota_UF <- dataset_frota_UF %>%
   group_by(UF, TIPO) %>%
   summarise(media = round(mean(diferenca, na.rm = TRUE), digits = 0))
 
+write_csv(media_aumento_frota_UF, 'media_aumento_frota_UF.csv')
+
+#Loop For para cada UF e Categoria de Veículo, e salva o resultado em plot interativo em html
 for (item_UF in lista_UF) {
   
   for(item_tipo in lista_tipo) {
@@ -36,9 +47,10 @@ for (item_UF in lista_UF) {
                         select(ds, y), 
                       yearly.seasonality = TRUE, 
                       weekly.seasonality = FALSE, 
-                      daily.seasonality = FALSE)
+                      daily.seasonality = FALSE, 
+                      seasonality.mode = "multiplicative")
     
-    periodos_futuros <- make_future_dataframe(modelo, 48, freq = "month", include_history = TRUE)
+    periodos_futuros <- make_future_dataframe(modelo, n_periodos, freq = "month", include_history = TRUE)
     
     projecao <- predict(modelo, periodos_futuros)
     
